c919bb133b4324ef1b9fdf298f71b044fc37485e
hpengfei <568347223@qq.com>
2017-11-24 18:07:58 +0800

    Initial commit
    dir init

---
+++ week_01/day03/login.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/10 10:50
+# @Author  : hpengfei
+# @File    : login.py
+# @Software: PyCharm Community Edition
+# 输入用户名密码
+# 认证成功后显示欢迎信息
+# 输错三次后锁定
+
+# 作业三：多级菜单
+# 三级菜单
+# 可依次选择进入各子菜单
+# 所需新知识点：列表、字典
+
+
+
+user = 'hello'
+passwd = 'world'
+
+user_input = input("Please input auth")
+
+
+
+
---
+++ week_01/day04/compare.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/10 10:51
+# @Author  : hpengfei
+# @File    : compare.py
+# @Software: PyCharm Community Edition
+
+a = int(input("a:"))
+b = int(input("b:"))
+c = int(input("c:"))
+# if a > b:
+#     print("a > b")
+# elif a < b:
+#     print("a < b")
+# else:
+#     print("a == b")
+
+if a > b > c:
+    print("a is max.")
+elif
+
